5902f9dc99d2f1c9fe968ca0e3d956c6962918ed
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2026-05-31 03:03:54 +0530

    refactor: update index.ts exports for new modular structure

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-export { RabbitMqBaseClass } from "./connection.js";
-export { RabbitMqQueueExchange } from "./exchange.js";
-export { RabbitProducer } from "./producer.js";
-export { RabbitConsumer } from "./consumer.js";
+export { type IRabbitConnection, RabbitSingleConnectionHandler } from "./connection/single.js";
+export { RabbitConnectionPoolHandler } from "./connection/pool.js";
+export { RabbitMqQueueExchange } from "./exchange/exchange.js";
+export { RabbitProducer } from "./producer/producer.js";
+export { RabbitConsumer } from "./consumer/consumer.js";
+export { RabbitLogger } from "./logger/logger.js";
 export type {
   ExchangeTopics,
   ExchangeOptions,
