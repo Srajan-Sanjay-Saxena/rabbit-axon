5ecb352e3d44363317afc43a220294f05a9d8547
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2026-05-31 17:36:13 +0530

    chore: export ISerializer, JsonSerializer, MsgpackSerializer, IdentitySerializer and ProducerOptions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 export { type IRabbitConnection, RabbitSingleConnectionHandler } from "./connection/single.js";
 export { RabbitConnectionPoolHandler } from "./connection/pool.js";
 export { RabbitMqQueueExchange } from "./exchange/exchange.js";
-export { RabbitProducer } from "./producer/producer.js";
+export { RabbitProducer, type ProducerOptions } from "./producer/producer.js";
 export { RabbitConsumer } from "./consumer/consumer.js";
 export { RabbitLogger } from "./logger/logger.js";
+export { type ISerializer, JsonSerializer, MsgpackSerializer, IdentitySerializer } from "./serializer/serializer.js";
 export type {
   ExchangeTopics,
   ExchangeOptions,
